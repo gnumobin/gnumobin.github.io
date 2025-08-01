e0a9898bcd0909a565a24aad1b99be5f144e22c6
gnumobin <gnumobin@gmail.com>
2025-08-01 17:44:41 +0330

    hamburgerMenu v1

---
+++ js/app.js
@@ -0,0 +1,23 @@
+const hamMenuBtn = document.getElementById("hamMenuBtn");
+const hamMenu = document.getElementById("hamMenu");
+const overlay = document.getElementById("overlay");
+
+hamMenuBtn.addEventListener("click", () => {
+  hamMenu.style.visibility = "visible";
+  hamMenu.style.transform = "translateX(0%)";
+
+  overlay.style.visibility = "visible";
+  overlay.style.transform = "translateX(0%)";
+
+  hamMenuBtn.classList.toggle("active");
+});
+
+overlay.addEventListener("click", () => {
+  hamMenu.style.visibility = "hidden";
+  hamMenu.style.transform = "translateX(-100%)";
+
+  overlay.style.visibility = "hidden";
+  overlay.style.transform = "translateX(200%)";
+
+  hamMenuBtn.classList.toggle("active");
+});
